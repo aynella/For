fd58931875aefeecf2f66e2b6c5c6766abd6c4ae
aynella <56473416+aynella@users.noreply.github.com>
2020-02-18 12:04:31 +0300

    Add files via upload

---
+++ f.py
@@ -0,0 +1,3 @@
+import math
+number=int(input())
+print(math.factorial(number))
---
+++ h.py
@@ -0,0 +1,12 @@
+p = int(input())
+a = '    _~_   '
+b = '   (o o)  '
+c = '  /  V  \  '
+d = ' /(  _  )\ '
+e = '   ^^ ^^  '
+
+print(a*p)
+print(b*p)
+print(c*p)
+print(d*p)
+print(e*p)
